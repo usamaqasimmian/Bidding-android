a8072fd6917faaf993ac7043902537cb79436c79
Usama Qasim Mian <usama1997qasim@gmail.com>
2021-04-12 04:16:56 +0500

    Added Login Api
    Added Login API and Verify Token
    Yet to Complete, but will still go to student form

--- OMS_2/app/src/main/java/com/example/oms_2/LoginPage.java
+++ OMS_2/app/src/main/java/com/example/oms_2/LoginPage.java
@@ -1,17 +1,34 @@
 package com.example.oms_2;
 
 import android.annotation.SuppressLint;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class LoginPage extends AppCompatActivity {
 
+    private static final String myApiKey = "dgcL8ghC8GPGJw9tcHK8fBKQgphtt7";
+    private static final String rootUrl = "https://fit3077.com/api/v1";
     EditText UserName;
     EditText Password;
     Button Login;
@@ -37,32 +54,57 @@ public class LoginPage extends AppCompatActivity {
     }
 
     @SuppressLint("SetTextI18n")
-    private void check(String Username, String Password){
-        if(Username.equals("admin") && (Password.equals("admin"))){
-            Intent intent = new Intent(com.example.oms_2.LoginPage.this, StudentLoggedIn.class);
-            LoginPage.this.startActivity(intent);
+    private void check(String username, String password){
+        String usersUrl = rootUrl + "/user/login";
+        final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        String json = "{" +
+                "\"userName\":\"" + username + "\"," +
+                "\"password\":\"" + password + "\"" +
+                "}";
+        RequestBody body = RequestBody.create(json, JSON);
 
-        }
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(usersUrl + "?jwt=true")
+                .header("Authorization",myApiKey)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+            }
 
-        else if (Username.equals("ted") && (Password.equals("1234"))) {
-            Intent intent = new Intent(com.example.oms_2.LoginPage.this, StudentLoggedIn.class);
-            LoginPage.this.startActivity(intent);
-
-        }
-
-        else if (Username.equals("tom") && (Password.equals("warehouse"))) {
-            Intent intent = new Intent(com.example.oms_2.LoginPage.this, StudentLoggedIn.class);
-            LoginPage.this.startActivity(intent);
-
-        }
-        else if (Username.equals("tutor") && (Password.equals("tutor"))){
-            Intent intent = new Intent(com.example.oms_2.LoginPage.this, TutorLoggedIn.class);
-            LoginPage.this.startActivity(intent);
-        }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
 
 
-        else{
-            Error.setText("Incorrect Password or Username");
-        }
+                    LoginPage.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject reader = new JSONObject(response.body().string());
+                                String jwt = reader.getString("jwt");
+                                VerifyToken(jwt);
+                                Error.setText(jwt);
+                            } catch (IOException | JSONException e) {
+                                e.printStackTrace();
+                            }
+                            //Intent intent = new Intent(com.example.oms_2.LoginPage.this, VerifyToken.class);
+                            //intent.putExtra("jwd", String.valueOf(request.url()));
+                            //LoginPage.this.startActivity(intent);
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private void VerifyToken(String jwt){
+        Intent intent = new Intent(com.example.oms_2.LoginPage.this, StudentLoggedIn.class);
+        LoginPage.this.startActivity(intent);
     }
 }
+

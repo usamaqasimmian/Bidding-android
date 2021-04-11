fc19b437a9c16a06082d8e04b141f8fc8931c5f1
Usama Qasim Mian <usama1997qasim@gmail.com>
2021-04-12 04:29:33 +0500

    Revert "Revert "Revert "merge"""
    This reverts commit 2ba8265e5b52f9109424e54e9d9dfa6e5f44b1e6.

---
+++ OMS_2/app/src/main/java/com/example/oms_2/TutorBidOpen.java
@@ -0,0 +1,15 @@
+package com.example.oms_2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorBidOpen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_bid_open);
+    }
+}
